b5702e035c9f827c8b53af36f4a0305d4a36e336
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2022-11-23 16:54:13 -0500

    Fix windows compilation

--- cmake/recipes/sweep_and_tiniest_queue.cmake
+++ cmake/recipes/sweep_and_tiniest_queue.cmake
@@ -10,8 +10,8 @@ option(STQ_WITH_CUDA "Enable CUDA Implementation" OFF) # get this through GPU_CC
 include(FetchContent)
 FetchContent_Declare(
     sweep_and_tiniest_queue
-    GIT_REPOSITORY https://github.com/dbelgrod/broadphase-gpu.git
-    GIT_TAG 48bfb9c4907ae07319574f4726cc451eb98055a3
+    GIT_REPOSITORY https://github.com/Ahdhn/broadphase-gpu.git
+    GIT_TAG 4cc48e5dc52f92109ad48ec94a0db5359704bec7
     GIT_SHALLOW FALSE
 )
 FetchContent_MakeAvailable(sweep_and_tiniest_queue)
